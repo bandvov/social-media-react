ed2e378833c512fd97614ad6fa37716e381f067a
bandvov <bandvov@gmail.com>
2025-01-02 09:57:24 +0200

    add translation

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import LoginForm from '../components/LoginForm';
+import React from "react";
+import LoginForm from "../components/LoginForm";
+import { Box, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
-const LoginPage = () => (
-  <div style={{ maxWidth: 400, margin: 'auto', padding: '1rem' }}>
-    <h1>Login</h1>
-    <LoginForm />
-  </div>
-);
+const LoginPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div style={{ maxWidth: 400, margin: "auto", padding: "1rem" }}>
+      <h1>{t("login")}</h1>
+      <LoginForm />
+      <Box
+        sx={{
+          mt: 2,
+          display: "grid",
+          gridTemplateColumns: "1fr auto",
+        }}
+      >
+        <Typography>Don't have account?</Typography>
+        <Link to="/registration">Sign up</Link>
+      </Box>
+    </div>
+  );
+};
 
 export default LoginPage;
--- src/pages/RegistrationPage.js
+++ src/pages/RegistrationPage.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import RegistrationForm from '../components/RegistrationForm';
+import React from "react";
+import RegistrationForm from "../components/RegistrationForm";
+import { Box, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
-const RegistrationPage = () => (
-  <div style={{ maxWidth: 400, margin: 'auto', padding: '1rem' }}>
-    <h1>registration</h1>
-    <RegistrationForm />
-  </div>
-);
+const RegistrationPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div style={{ maxWidth: 400, margin: "auto", padding: "1rem" }}>
+      <h1>{t("registration")}</h1>
+      <RegistrationForm />
+      <Box
+        sx={{
+          mt: 2,
+          display: "grid",
+          gridTemplateColumns: "1fr auto",
+        }}
+      >
+        <Typography>Have account?</Typography>
+        <Link to="/login">Login in</Link>
+      </Box>
+    </div>
+  );
+};
 
 export default RegistrationPage;
