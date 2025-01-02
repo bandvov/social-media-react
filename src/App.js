246fafd153d127c4ad4c18ce7b33c634081c9d83
bandvov <bandvov@gmail.com>
2025-01-02 10:14:34 +0200

    get rid of extra imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "./styles/theme";
 import LoginPage from "./pages/LoginPage";
 import "./i18n";
 import RegistrationPage from "./pages/RegistrationPage";
@@ -9,17 +7,15 @@ import Layout from "./components/Layout";
 import ProfilePage from "./pages/ProfilePage";
 
 const App = () => (
-  <ThemeProvider theme={theme}>
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/registration" element={<RegistrationPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-        </Routes>
-      </Layout>
-    </Router>
-  </ThemeProvider>
+  <Router>
+    <Layout>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </Layout>
+  </Router>
 );
 
 export default App;
