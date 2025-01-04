c8eb3e9ed5cb79a0d3b60839abdb77d34b718622
bandvov <bandvov@gmail.com>
2025-01-04 11:56:49 +0200

    add user posts and profile page

--- src/pages/PostsPage.js
+++ src/pages/PostsPage.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from "react";
+import UserPosts from "../components/Posts/IserPosts";
 
 export default function PostsPage() {
-  return (
-    <div>PostsPage</div>
-  )
+  return <UserPosts />;
 }
