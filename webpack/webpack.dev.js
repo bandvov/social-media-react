914a0ce6680d1f69813c7e63253a0d65ff42e1a8
bandvov <bandvov@gmail.com>
2025-01-08 19:21:16 +0200

    update webpack config

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const fs = require("fs");
+const { LoaderOptionsPlugin } = require("webpack");
 
 module.exports = merge(common, {
   // Set mode to development for unoptimized build
@@ -22,4 +23,19 @@ module.exports = merge(common, {
     port: 3000, // Port to run the development server
     historyApiFallback: true, // Redirect 404s to index.html for React Router
   },
+  plugins: [
+    new LoaderOptionsPlugin({
+      // test: /\.xxx$/, // may apply this only for some modules
+      options: {
+        client: {
+          // Shows a full-screen overlay in the browser when there are compiler errors or warnings
+          overlay: {
+            errors: true,
+            warnings: true,
+          },
+          progress: true, // Prints compilation progress in percentage in the browser.
+        },
+      },
+    }),
+  ],
 });
